444fe10fc524c1b36ec7a5f97cc0abb7d5f17d09
Rukayat Jimoh <jimohadedamola925@gmail.com>
2023-12-08 20:11:26 -0500

    added cert

--- src/components/about_me.component.js
+++ src/components/about_me.component.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery"
 import "bootstrap/dist/js/bootstrap";
 import { text } from './textfiles/aboutme.json';
+import cloud_eng_cert from "../images/google_cloud_cert.jpg";
 
 export default class AboutMe extends Component{
     render(){
@@ -15,12 +16,28 @@ export default class AboutMe extends Component{
                 <style>{'body { background-image: url(../images/1.jpg); }'}</style>
             </Helmet> */}
                 <h1>Rukayat Adedamola Jimoh</h1>
-                <div class="card">
-                    <div class="card-body">
+                <div className="card">
+                    <div className="card-body">
                         {text}
                     </div>
                 </div>
                 <p>To learn more about me or setup a live interview. <Link to="/contactme"><strong>Email</strong></Link> or Call me at (289)808-0532.</p>
+                <div className='card'>
+                    <div className='card-body'>
+                        <div className='card-title'><h1 style={ { fontWeight: 'bold', color: 'black'}}>EDUCATION</h1></div>
+                        <div className='card-text'>
+                            <h3 style={ { fontWeight: 'bold', color: 'black' }}>ONTARIO TECHNOLOGY UNIVERSITY                     2017-2021</h3>
+                            <h5>Bachelor's of Engineering - Software Engineering</h5>
+                        </div>
+                    </div>
+                </div>
+                <br/><br/>
+                <div className='card' style={{width: "18rem"}}>
+                    <img src={cloud_eng_cert} className='card-img-top' alt='Cloud Engineer Certificate'/>
+                    <div class="card-body">
+                        <p class="card-text" style={{ color: 'black'}}>Google Associate Cloud Engineer Certificate</p>
+                    </div>
+                </div>
             </div>
         );
     }
